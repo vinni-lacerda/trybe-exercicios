b8e1bd33458ed20c781f52b6c4a8b26cc933caab
VinniLs <vini.lacerda@hotmail.com>
2022-11-25 18:27:24 -0300

    requisitos 1, 2 e 3 feitos

---
+++ 1 - fundamentos/secao-04/dia-02/exercicio-do-dia.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for(let i=0; i<numbers.length; i++){
+    // console.log(numbers[i])
+     sumResult = sum += numbers[i];
+     media = sumResult/numbers.length
+}
+
+console.log(sumResult)
+console.log(media)
