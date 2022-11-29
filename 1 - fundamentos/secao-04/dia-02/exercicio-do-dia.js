7561cc0349d0fc5999f8fbadfd16d7eaa47c2e88
VinniLs <vini.lacerda@hotmail.com>
2022-11-29 14:53:58 -0300

    Requisito 7 feito

--- 1 - fundamentos/secao-04/dia-02/exercicio-do-dia.js
+++ 1 - fundamentos/secao-04/dia-02/exercicio-do-dia.js
@@ -2,6 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let maior = 0;
 let impares = 0;
+let menor = numbers[0];
 // let contador = 0;
 
 // for(let i=0; i<numbers.length; i++){
@@ -18,22 +19,24 @@ let impares = 0;
 // console.log(sum)
 // console.log(media)
 
+// for (let i = 0; i<numbers.length; i++){
+//     if(maior<numbers[i]){
+//         maior = numbers[i]
+//     }    
+
+// }
+// console.log(maior);
+
+
+//  console.log(`Numero de impares: ${impares}`)
+
+// if(impares == 0){
+//     console.log('Nenhum valor impar encontrado')
+// }
+
 for (let i = 0; i<numbers.length; i++){
-    if(maior<numbers[i]){
-        maior = numbers[i]
+    if(menor > numbers[i]){
+        menor = numbers[i]
     }    
-
-}
-console.log(maior);
-
-for (let i = 0; i<numbers.length; i++){
-    if(numbers[i] % 2 != 0 ){
-        impares++
-    }    
-
  }  
- console.log(`Numero de impares: ${impares}`)
-
-if(impares == 0){
-    console.log('Nenhum valor impar encontrado')
-}
+ console.log(menor)
