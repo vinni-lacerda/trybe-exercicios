21cb14765cdbbb99d0d17945b906888c8931348a
VinniLs <vini.lacerda@hotmail.com>
2022-11-29 14:36:18 -0300

    5° requisito feito

--- 1 - fundamentos/secao-04/dia-02/exercicio-do-dia.js
+++ 1 - fundamentos/secao-04/dia-02/exercicio-do-dia.js
@@ -1,11 +1,27 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let maior = 0;
+// let contador = 0;
 
-for(let i=0; i<numbers.length; i++){
-    // console.log(numbers[i])
-     sumResult = sum += numbers[i];
-     media = sumResult/numbers.length
+// for(let i=0; i<numbers.length; i++){
+//     console.log(numbers[i])
+//      sum += numbers[i];
+//      media = sum/numbers.length
+
+// }
+// if(media > 20){
+//     console.log('Valor maior que 20')
+// }else{
+//     console.log('Valor menor ou igual a 20')
+// }
+// console.log(sum)
+// console.log(media)
+
+for (let i = 0; i<numbers.length; i++){
+    if(maior<numbers[i]){
+        maior = numbers[i]
+    }    
+
 }
-
-console.log(sumResult)
-console.log(media)
+console.log(maior);
+// console.log(contador)
