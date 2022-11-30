e4ca9b26c677eb03fc4f7db247285fa4ba918282
VinniLs <vini.lacerda@hotmail.com>
2022-11-30 11:18:22 -0300

    Resolve colocar a palavra ao contrario, requisito 2

--- 1 - fundamentos/secao-04/dia-03/exercicio.js
+++ 1 - fundamentos/secao-04/dia-03/exercicio.js
@@ -1,10 +1,18 @@
-// O fatorial é representado pelo sinal !
-// 4! = 4 x 3 x 2 x 1 = 24
 
-let fatorial = 1;
+// let fatorial = 1;
 
-for(let index = 10; index>0; index -= 1){
-    fatorial = fatorial * index
+// for(let index = 10; index>0; index -= 1){
+//     fatorial = fatorial * index
+// }
+
+// console.log(fatorial)
+
+
+let word = 'tryber';
+let reverseWord = [];
+
+for (let index = 5; index >= 0; index--) {
+    reverseWord.push(word[index])
 }
 
-console.log(fatorial)
+console.log(reverseWord.join(''))
