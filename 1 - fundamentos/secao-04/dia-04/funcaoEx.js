edae67facdea89f866c69ef61833560d24152798
VinniLs <vini.lacerda@hotmail.com>
2022-12-01 11:13:01 -0300

    terceiro requisito do exericio feito

--- 1 - fundamentos/secao-04/dia-04/funcaoEx.js
+++ 1 - fundamentos/secao-04/dia-04/funcaoEx.js
@@ -25,3 +25,21 @@ function maiorIndice(arrayTeste){
 }
 
 console.log(maiorIndice(arrayTeste))
+
+
+let arrayTeste2 = [2, 4, 6, 7, 10, 0, -3];
+
+function menorIndice(arrayTeste){
+    let menorIndice; 
+    let menor = arrayTeste2[0]
+
+    for(let index = 0; index<arrayTeste.length; index += 1){
+        if(menor > arrayTeste[index]){
+            menor = arrayTeste[index]
+            menorIndice = index
+        }
+    }
+    return menorIndice
+}
+
+console.log(menorIndice(arrayTeste2))
