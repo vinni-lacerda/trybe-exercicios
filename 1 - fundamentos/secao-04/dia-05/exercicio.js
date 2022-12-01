343eafce128b01ffe0f8cc9165ae7b9673756aee
VinniLs <vini.lacerda@hotmail.com>
2022-12-01 18:52:33 -0300

    Requisito 3 e 4 feitos

--- 1 - fundamentos/secao-04/dia-05/exercicio.js
+++ 1 - fundamentos/secao-04/dia-05/exercicio.js
@@ -8,17 +8,34 @@
 
 
   // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
-  const pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
+//   const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
 
-  //   pessoa = {
-      //     nome: 'Luna',
-      //     idade: 19
-      //   } // Altere essa estrutura para corrigir o erro.
+//   //   pessoa = {
+//       //     nome: 'Luna',
+//       //     idade: 19
+//       //   } // Altere essa estrutura para corrigir o erro.
 
-    pessoa.nome = 'Luna';    
-    pessoa.idade = 19;
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+//     pessoa.nome = 'Luna';    
+//     pessoa.idade = 19;
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hamburguer';
+// console.log(favoriteFood);
+
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá ${name} ${lastName} !`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
