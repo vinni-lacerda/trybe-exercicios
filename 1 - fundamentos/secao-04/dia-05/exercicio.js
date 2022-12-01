72d0d2eb8797b3552b1299424c61a9f75b727e28
VinniLs <vini.lacerda@hotmail.com>
2022-12-01 18:48:11 -0300

    Requisito 1(trocar o valor do objeto) feito.

--- 1 - fundamentos/secao-04/dia-05/exercicio.js
+++ 1 - fundamentos/secao-04/dia-05/exercicio.js
@@ -1,7 +1,24 @@
-function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
-    }
-    // console.log('Idade fora do for:', idade) // retire essa linha ao implementar o tipo de variável correta.
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+//     // console.log('Idade fora do for:', idade) // retire essa linha ao implementar o tipo de variável correta.
+//   }
+//   imprimeIdade()
+
+
+  // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+  const pessoa = {
+    nome: 'Henri',
+    idade: 20
   }
-  imprimeIdade()
+
+  //   pessoa = {
+      //     nome: 'Luna',
+      //     idade: 19
+      //   } // Altere essa estrutura para corrigir o erro.
+
+    pessoa.nome = 'Luna';    
+    pessoa.idade = 19;
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
