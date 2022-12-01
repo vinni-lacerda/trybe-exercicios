6d9a0f3fd773148ccb0d475e936763d4a821ba4a
VinniLs <vini.lacerda@hotmail.com>
2022-12-01 10:45:56 -0300

    primeiro requisito do exercicio feito

---
+++ 1 - fundamentos/secao-04/dia-04/funcaoEx.js
@@ -0,0 +1,10 @@
+// let str = 'arara'
+function verificaPalindromo (str){
+    if (str == str.split('').reverse().join('')){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(verificaPalindromo('sim'))
