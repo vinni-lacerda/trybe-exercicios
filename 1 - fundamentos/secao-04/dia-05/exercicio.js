ef678f0db52a9a4ae64a83759249c72ed2a315fe
VinniLs <vini.lacerda@hotmail.com>
2022-12-01 18:58:14 -0300

    Requisito 5 feito

--- 1 - fundamentos/secao-04/dia-05/exercicio.js
+++ 1 - fundamentos/secao-04/dia-05/exercicio.js
@@ -29,13 +29,22 @@
 // console.log(favoriteFood);
 
 
-const name = 'Adriana';
-const lastName = 'Soares';
-console.log(`Olá ${name} ${lastName} !`);
-function soma(a,b) {
-  let resultado = a + b;
-  return resultado;
-}
-let a = 3;
-let b = 5;
-console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+// const name = 'Adriana';
+// const lastName = 'Soares';
+// console.log(`Olá ${name} ${lastName} !`);
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+
+// function numeroAleatorio() {
+//     return Math.random()
+//   }
+//   console.log(numeroAleatorio());
+  
+const numeroAleatorio = () => {return Math.random()  }
+  console.log(numeroAleatorio());
+  
