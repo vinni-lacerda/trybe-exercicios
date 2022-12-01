48dc64c990c0f562e9fa98dcbcc85f39cbb1997f
VinniLs <vini.lacerda@hotmail.com>
2022-12-01 11:34:53 -0300

    quarto requisito do exericio feito

--- 1 - fundamentos/secao-04/dia-04/funcaoEx.js
+++ 1 - fundamentos/secao-04/dia-04/funcaoEx.js
@@ -1,45 +1,65 @@
 // let str = 'arara'
-function verificaPalindromo (str){
-    if (str == str.split('').reverse().join('')){
-        return true;
-    }else{
-        return false;
+// function verificaPalindromo (str){
+//     if (str == str.split('').reverse().join('')){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// // console.log(verificaPalindromo('arara'))
+
+
+// let arrayTeste = [2, 3, 6, 7, 10, 1]
+// function maiorIndice(arrayTeste){
+//     let maiorIndice; 
+//     let maior = arrayTeste[0]
+
+//     for(let index = 0; index<arrayTeste.length; index += 1){
+//         if(maior < arrayTeste[index]){
+//             maior = arrayTeste[index]
+//             maiorIndice = index
+//         }
+//     }
+//     return maiorIndice
+// }
+
+// console.log(maiorIndice(arrayTeste))
+
+
+// let arrayTeste2 = [2, 4, 6, 7, 10, 0, -3];
+
+// function menorIndice(arrayTeste){
+//     let menorIndice; 
+//     let menor = arrayTeste2[0]
+
+//     for(let index = 0; index<arrayTeste.length; index += 1){
+//         if(menor > arrayTeste[index]){
+//             menor = arrayTeste[index]
+//             menorIndice = index
+//         }
+//     }
+//     return menorIndice
+// }
+
+// console.log(menorIndice(arrayTeste2))
+
+
+
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']
+
+function biggestName(nomes){
+    let firstName = nomes[0]
+
+    for (let index = 0; index < nomes.length; index++) {
+
+        if(nomes[index].length > firstName.length){
+            firstName = nomes[index]
+       }
     }
+
+    return firstName
+
 }
 
-// console.log(verificaPalindromo('arara'))
-
-
-let arrayTeste = [2, 3, 6, 7, 10, 1]
-function maiorIndice(arrayTeste){
-    let maiorIndice; 
-    let maior = arrayTeste[0]
-
-    for(let index = 0; index<arrayTeste.length; index += 1){
-        if(maior < arrayTeste[index]){
-            maior = arrayTeste[index]
-            maiorIndice = index
-        }
-    }
-    return maiorIndice
-}
-
-console.log(maiorIndice(arrayTeste))
-
-
-let arrayTeste2 = [2, 4, 6, 7, 10, 0, -3];
-
-function menorIndice(arrayTeste){
-    let menorIndice; 
-    let menor = arrayTeste2[0]
-
-    for(let index = 0; index<arrayTeste.length; index += 1){
-        if(menor > arrayTeste[index]){
-            menor = arrayTeste[index]
-            menorIndice = index
-        }
-    }
-    return menorIndice
-}
-
-console.log(menorIndice(arrayTeste2))
+console.log(biggestName(nomes))
