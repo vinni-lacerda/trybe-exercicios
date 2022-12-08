f5216613b3a8e3257cc44b17dd23890886457f64
VinniLs <vini.lacerda@hotmail.com>
2022-12-08 15:49:39 -0300

    Commit teste

--- 1 - fundamentos/secao-04/dia-05/exercicio.js
+++ 1 - fundamentos/secao-04/dia-05/exercicio.js
@@ -53,9 +53,18 @@
 //   let nome = 'Ivan';
 //   console.log(hello(nome));
 
-const nomeCompleto = (nome, sobrenome) => {return `${nome} ${sobrenome}`}
 
-  let nome = 'Ivan';
-  let sobrenome = 'Pires';
-  console.log(nomeCompleto(nome, sobrenome));
 
+
+// const nomeCompleto = (nome, sobrenome) => {return `${nome} ${sobrenome}`}
+
+//   let nome = 'Ivan';
+//   let sobrenome = 'Pires';
+//   console.log(nomeCompleto(nome, sobrenome));
+
+
+let speed = 90;
+
+const speedCar = (speed) => { (speed >= 120) ?  `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`; };
+
+console.log(speedCar(speed));
